e9a2b6758c39242694bd782f57939d2c52cc7364
CEISA3 <CEISA3@CEISA3-HP>
2014-08-26 13:27:31 +0700

    Merge branch 'master' of https://github.com/artivisi/training-djbc-2014-01

--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/domain/Supplier.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/domain/Supplier.java
@@ -9,5 +9,58 @@ package com.muhardin.endy.training.pos.domain;
  * @author Lenovo
  */
 public class Supplier {
-    
+    private Integer id;
+    private Integer kode;
+    private String nama;
+    private String telepon;
+    private String kota;    
+    private String alamat;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getKode() {
+        return kode;
+    }
+
+    public void setKode(Integer kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
 }
