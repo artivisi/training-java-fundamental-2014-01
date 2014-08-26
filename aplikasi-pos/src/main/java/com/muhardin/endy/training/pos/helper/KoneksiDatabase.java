a864afe7ba319b3b82de2447e3b743463f430058
senbesu <senbesu@gmail.com>
2014-08-26 13:52:16 +0700

    Merge branch 'master' of https://github.com/artivisi/training-djbc-2014-01

--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/helper/KoneksiDatabase.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/helper/KoneksiDatabase.java
@@ -20,7 +20,7 @@ public class KoneksiDatabase {
         
         // 2. Connect ke database
             koneksi = DriverManager
-                    .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+                    .getConnection("jdbc:oracle:thin:@localhost:1521:XE, pelatihan, 1");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(KoneksiDatabase.class.getName()).log(Level.SEVERE, null, ex);
         }
