7a74358de81ec831dd2c53cd7eee6ea5f73d62fe
chamaludi <ludiantochamal@yahoo.com>
2014-08-26 10:44:17 +0700

    penjualan domain dan dao

---
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/dao/PenjualanDao.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.muhardin.endy.training.pos.dao;
+
+import com.muhardin.endy.training.pos.domain.Penjualan;
+import java.util.List;
+
+/**
+ *
+ * @author CEISA12
+ */
+public class PenjualanDao {
+    public void simpanJual(Penjualan j) {
+    
+    }
+    
+    public void batalJual(Penjualan j) {
+    
+    }
+    
+    public void diskonJual(Penjualan j) {
+    
+    }
+    
+    public Penjualan cariById(Integer id) {
+        return null;
+    }
+    
+    public List<Penjualan> listPenjualan() {
+        return null;
+    }
+}
--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/domain/Penjualan.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/domain/Penjualan.java
@@ -6,10 +6,85 @@
 
 package com.muhardin.endy.training.pos.domain;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  *
  * @author CEISA12
  */
 public class Penjualan {
+    private Integer id;
+    private String namaBarang;
+    private String namaPembeli;
+    private BigDecimal jmlBarang;
+    private BigDecimal harga;
+    private BigDecimal stockBarang;
+    private Integer diskon;
+    private Date tglPenjualan;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public String getNamaPembeli() {
+        return namaPembeli;
+    }
+
+    public void setNamaPembeli(String namaPembeli) {
+        this.namaPembeli = namaPembeli;
+    }
+
+    public BigDecimal getJmlBarang() {
+        return jmlBarang;
+    }
+
+    public void setJmlBarang(BigDecimal jmlBarang) {
+        this.jmlBarang = jmlBarang;
+    }
+
+    public BigDecimal getHarga() {
+        return harga;
+    }
+
+    public void setHarga(BigDecimal harga) {
+        this.harga = harga;
+    }
+
+    public BigDecimal getStockBarang() {
+        return stockBarang;
+    }
+
+    public void setStockBarang(BigDecimal stockBarang) {
+        this.stockBarang = stockBarang;
+    }
+
+    public Integer getDiskon() {
+        return diskon;
+    }
+
+    public void setDiskon(Integer diskon) {
+        this.diskon = diskon;
+    }
+
+    public Date getTglPenjualan() {
+        return tglPenjualan;
+    }
+
+    public void setTglPenjualan(Date tglPenjualan) {
+        this.tglPenjualan = tglPenjualan;
+    }
     
 }
