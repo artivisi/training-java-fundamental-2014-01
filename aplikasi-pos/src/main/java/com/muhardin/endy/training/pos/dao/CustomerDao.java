0f4104f86dbf005b9bcb779ba3ffebb1c4c5910a
CEISA-DJBC <CEISA-DJBC@CEISA18-HP>
2014-08-26 10:24:38 +0700

    Penambahan Customer

---
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/dao/CustomerDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.muhardin.endy.training.pos.dao;
+
+/**
+ *
+ * @author CEISA-DJBC
+ */
+public class CustomerDao {
+    
+}
