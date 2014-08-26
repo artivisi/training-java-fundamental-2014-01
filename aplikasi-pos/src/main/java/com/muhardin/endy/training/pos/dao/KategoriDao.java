f48d9ba3e5eb8509999a3ced199a7c2c98f87891
dhayatul <dohanum@gmail.com>
2014-08-26 10:37:48 +0700

    implement getter/setter on Kategori & add KategoriDemo.java

--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/dao/KategoriDao.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/dao/KategoriDao.java
@@ -1,5 +1,22 @@
 package com.muhardin.endy.training.pos.dao;
 
+import com.muhardin.endy.training.pos.domain.Kategori;
+import java.util.List;
+
 public class KategoriDao {
+    public void simpan(Kategori k){
+        
+    }
     
+    public void hapus(Kategori k){
+        
+    }
+    
+    public Kategori cariById(Integer id){
+        return null;
+    }
+    
+    public List<Kategori> semuaKategori(){
+        return null;
+    }
 }
