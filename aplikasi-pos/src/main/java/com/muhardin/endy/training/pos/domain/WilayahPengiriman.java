fcbb131c9395a9120171a7ffa632f624be69ecb7
estinovita <aza.spirit@gmail.com>
2014-08-26 10:45:04 +0700

    isi

--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/domain/WilayahPengiriman.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/domain/WilayahPengiriman.java
@@ -4,10 +4,48 @@
  */
 package com.muhardin.endy.training.pos.domain;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author CEISA-DJBC
  */
 public class WilayahPengiriman {
-    
+    private Integer id;
+    private String kode;
+    private String namaWilayah;
+    private BigDecimal ongkosKirim;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNamaWilayah() {
+        return namaWilayah;
+    }
+
+    public void setNamaWilayah(String namaWilayah) {
+        this.namaWilayah = namaWilayah;
+    }
+
+    public BigDecimal getOngkosKirim() {
+        return ongkosKirim;
+    }
+
+    public void setOngkosKirim(BigDecimal ongkosKirim) {
+        this.ongkosKirim = ongkosKirim;
+    }
+     
 }
