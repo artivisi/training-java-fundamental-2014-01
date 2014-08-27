5ee67db75c468f918281ec36dd9cb731f6e4cdfb
dhayatul <dohanum@gmail.com>
2014-08-27 13:56:12 +0700

    Validation on Kategori

--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/demo/KategoriImporterDemo.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/demo/KategoriImporterDemo.java
@@ -6,6 +6,8 @@
 package com.muhardin.endy.training.pos.demo;
 
 import com.muhardin.endy.training.pos.domain.Kategori;
+import com.muhardin.endy.training.pos.importer.HasilImportKategori;
+import com.muhardin.endy.training.pos.importer.ImportError;
 import com.muhardin.endy.training.pos.importer.KategoriImporter;
 import java.io.File;
 import java.util.List;
@@ -21,13 +23,20 @@ public class KategoriImporterDemo {
 
         KategoriImporter pi = new KategoriImporter();
 
-        List<Kategori> hasil = pi.importFile(f);
-        System.out.println("======== Hasil : " + hasil.size() + " records ===========");
-        for (Kategori kategori : hasil) {
+        HasilImportKategori hasil = pi.importFile(f);
+        System.out.println("======== Hasil : " + hasil.getData().size() + " records ===========");
+        for (Kategori kategori : hasil.getData()) {
             System.out.println("ID : " + kategori.getId());
             System.out.println("Kode : " + kategori.getKode());
             System.out.println("Nama : " + kategori.getNama());
             System.out.println("Definisi : " + kategori.getDefinisi());
+        }
+        
+        System.out.println("=========== Gagal : "+hasil.getDaftarError().size()+" baris ========");
+        for (ImportError err : hasil.getDaftarError()) {
+            System.out.println("Baris : "+err.getBaris());
+            System.out.println("Keterangan : "+err.getKeterangan());
+            System.out.println("Data : "+err.getData());
         }
     }
 }
