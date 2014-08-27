cc9983d55b6e956370436ec3d6b7e666316ddcef
myusrul <myhan99@gmail.com>
2014-08-27 11:24:45 +0700

    Commit region Importesting ciusss lhoh

--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/demo/RegionImportTesting.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/demo/RegionImportTesting.java
@@ -6,6 +6,7 @@
 
 package com.muhardin.endy.training.pos.demo;
 
+import com.muhardin.endy.training.pos.dao.RegionDao;
 import com.muhardin.endy.training.pos.domain.Region;
 import com.muhardin.endy.training.pos.importer.RegionImport;
 import java.io.File;
@@ -20,8 +21,23 @@ public class RegionImportTesting {
         File f = new File("src/main/resources/region.csv");
         
         RegionImport impor = new RegionImport();
-        
+        RegionDao dao = new RegionDao();
         List<Region> hasil = impor.importFile(f);
         System.out.println("======== Hasil : "+hasil.size()+" records ===========");
+        for (Region region : hasil) {
+            shoutout(region);
+            dao.simpan(region);
+            
+        }
+    }
+
+    private static void shoutout(Region region) {
+        System.out.println("ID "+region.getId());
+        System.out.println("Kode "+region.getIdRegion());
+        System.out.println("Provinsi "+region.getProvinsi());
+        System.out.println("Kota "+region.getKota());
+        System.out.println("Kecamatan "+region.getKecamatan());
+        System.out.println("Desa "+region.getDesa());
+        System.out.println("Tanggal buat "+region.getCreated());
     }
 }
