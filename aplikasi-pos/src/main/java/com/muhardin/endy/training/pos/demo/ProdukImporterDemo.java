74afb243dfe747a7cb6d3e238ac82bb5d4fcd5eb
Endy Muhardin <endy.muhardin@gmail.com>
2014-08-27 11:49:04 +0700

    validasi import

--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/demo/ProdukImporterDemo.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/demo/ProdukImporterDemo.java
@@ -1,9 +1,10 @@
 package com.muhardin.endy.training.pos.demo;
 
 import com.muhardin.endy.training.pos.domain.Produk;
+import com.muhardin.endy.training.pos.importer.HasilImportProduk;
+import com.muhardin.endy.training.pos.importer.ImportError;
 import com.muhardin.endy.training.pos.importer.ProdukImporter;
 import java.io.File;
-import java.util.List;
 
 public class ProdukImporterDemo {
     public static void main(String[] args) {
@@ -11,14 +12,21 @@ public class ProdukImporterDemo {
         
         ProdukImporter pi = new ProdukImporter();
         
-        List<Produk> hasil = pi.importFile(f);
-        System.out.println("======== Hasil : "+hasil.size()+" records ===========");
-        for (Produk produk : hasil) {
+        HasilImportProduk hasil = pi.importFile(f);
+        System.out.println("======== Sukses : "+hasil.getData().size()+" records ===========");
+        for (Produk produk : hasil.getData()) {
             System.out.println("ID : "+produk.getId());
             System.out.println("Kode : "+produk.getKode());
             System.out.println("Nama : "+produk.getNama());
             System.out.println("Harga : "+produk.getHarga());
             System.out.println("Tanggal : "+produk.getTanggalKadaluarsa());
+        }
+        
+        System.out.println("=========== Gagal : "+hasil.getDaftarError().size()+" baris ========");
+        for (ImportError err : hasil.getDaftarError()) {
+            System.out.println("Baris : "+err.getBaris());
+            System.out.println("Keterangan : "+err.getKeterangan());
+            System.out.println("Data : "+err.getData());
         }
     }
 }
