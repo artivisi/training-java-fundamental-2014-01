70730010307875d0d5c48306a67d5023ff26bd46
myusrul <myhan99@gmail.com>
2014-08-27 14:14:14 +0700

    Revisi Region (Cius Miapaha!!!)

--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/dao/RegionDao.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/dao/RegionDao.java
@@ -10,7 +10,10 @@ import com.muhardin.endy.training.pos.domain.Region;
 import com.muhardin.endy.training.pos.helper.KoneksiDatabase;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +25,9 @@ import java.util.logging.Logger;
 public class RegionDao{
     
     public static final String SQL_INSERT = "insert into region (id,idregion,provinsi,kota,kecamatan,desa,created) values (?,?,?,?,?,?,?)";
+    private static final String SQL_DELETE = "delete from region where id = ?";
+    private static final String SQL_SELECT_BY_ID = "select * from region where id = ?";
+    private static final String SQL_SELECT_ALL = "select * from region";
     
     public void simpan(Region reg) {
             try{
@@ -48,20 +54,75 @@ public class RegionDao{
            
     }
 
-    public void update(Region reg) throws Exception {
+    public void update(Region reg) {
             
      }
 
-     public void delete(Region reg) throws Exception {
-            
+     public void delete(Region reg) {
+         try{
+            Connection koneksi = KoneksiDatabase.bukaKoneksi();
+            PreparedStatement ps = koneksi.prepareStatement(SQL_DELETE);
+            ps.setInt(1, reg.getId());
+            int hasil = ps.executeUpdate();
+            System.out.println(hasil + " record berhasil dihapus");
+            KoneksiDatabase.tutupKoneksi(koneksi);
+            } 
+         catch (SQLException ex) {
+            Logger.getLogger(RegionDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
      }
     
         
     public Region cariById(Integer id){
-            return null;
+       try {
+            Connection conn = KoneksiDatabase.bukaKoneksi();
+            PreparedStatement ps = conn.prepareStatement(SQL_SELECT_BY_ID);
+            ps.setInt(1, id);
+            ResultSet hasil = ps.executeQuery();
+
+            if (hasil.next()) {
+                Region p = konversiResultSetKeProduk(hasil);
+                return p;
+            }
+
+            KoneksiDatabase.tutupKoneksi(conn);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdukDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;     
+        
     }
     
-    public List<Region> semuaProduk(){
-            return null;
+    public List<Region> semuaRegion(){
+        List<Region> hasil = new ArrayList<Region>(); 
+         try {
+            Connection conn = KoneksiDatabase.bukaKoneksi();
+
+            PreparedStatement ps = conn.prepareStatement(SQL_SELECT_ALL);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Region p = konversiResultSetKeProduk(rs);
+                hasil.add(p);
+            }
+
+            KoneksiDatabase.tutupKoneksi(conn);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegionDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return hasil;
+    }
+
+    private Region konversiResultSetKeProduk(ResultSet hasil) throws SQLException{
+        Region reg = new Region();
+        reg.setId(hasil.getInt("id"));
+        reg.setIdRegion(hasil.getString("idregion"));
+        reg.setProvinsi(hasil.getString("provinsi"));
+        reg.setKota(hasil.getString("kota"));
+        reg.setKecamatan(hasil.getString("kecamatan"));
+        reg.setDesa(hasil.getString("desa"));
+        reg.setCreated(new Date(hasil.getDate("created").getTime()));
+        
+        return reg;
     }
 }
--- aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/demo/RegionTest.java
+++ aplikasi-pos/src/main/java/com/muhardin/endy/training/pos/demo/RegionTest.java
@@ -5,26 +5,48 @@ import com.muhardin.endy.training.pos.domain.Region;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class RegionTest {
 
     public static void main(String[] args) {
         Format tanggalan = new SimpleDateFormat("dd-MM-yyyy");
         RegionDao re = new RegionDao();
-        for (int i = 1; i <= 10; i++) {
+      //  for (int i = 1; i <= 10; i++) {
             Region reg = new Region();
-            reg.setId(i);
-            reg.setIdRegion("0001NAD0" + i);
-            reg.setProvinsi("NAD" + i);
-            reg.setKota("Meulaboh" + i);
-            reg.setKecamatan("Aserehe" + i);
-            reg.setDesa("Ohey" + i);
+      //      reg.setId(i);
+            reg.setId(new Random().nextInt());
+//            reg.setIdRegion("0001NAD0" + i);
+//            reg.setProvinsi("NAD" + i);
+//            reg.setKota("Meulaboh" + i);
+//            reg.setKecamatan("Aserehe" + i);
+//            reg.setDesa("Ohey" + i);
+//            reg.setCreated(new Date());
+            reg.setIdRegion("0001NAD0" );
+            reg.setProvinsi("NAD" );
+            reg.setKota("Meulaboh" );
+            reg.setKecamatan("Aserehe" );
+            reg.setDesa("Ohey" );
             reg.setCreated(new Date());
 
             System.out.println("----------------------------------");
             re.simpan(reg);
             tampilin(reg, tanggalan);
+       // }
+        
+       Region rex = re.cariById(reg.getId());
+            tampilin(rex, tanggalan);
+        
+        System.out.println("=====================");
+        List<Region> semua = re.semuaRegion();
+        for (Region region : semua) {
+            tampilin(region,tanggalan);
+            System.out.println("-------------------------");
+
         }
+        System.out.println("=====================");
+    
 
     }
 
